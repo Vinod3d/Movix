12fe297638bd1a5e54de16493e5047940c03ff42
Vinod Chandra <vinodchandra979@gmail.com>
2023-09-09 01:51:11 +0530

    Trending sections

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css'
 import {fetchDataFromApi} from "./utils/api"
-import { getApiConfiguration } from './store/homeSlice';
+import { getApiConfiguration, getGenres } from './store/homeSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
@@ -22,6 +22,7 @@ function App() {
 
   useEffect(()=>{
     fetchApiConfig();
+    genresCall()
   }, []);
 
   const fetchApiConfig = () =>{
@@ -35,6 +36,24 @@ function App() {
       
       dispatch(getApiConfiguration(url))
     });
+  };
+
+  const genresCall = async () => {
+    let promises = []
+    let endPoint = ["tv", "movie"]
+    let allGenres = {}
+
+    endPoint.forEach((url) =>{
+      promises.push(fetchDataFromApi(`/genre/${url}/list`))
+    })
+
+    const data = await Promise.all(promises)
+    console.log(data)
+    data.map(({genres})=>{
+      return genres.map((item)=> (allGenres[item.id] = item))
+    });
+
+    dispatch(getGenres(allGenres ))
   };
 
   return (
--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -20,20 +20,66 @@ const Carousel = ({data, loading}) => {
     const {url} = useSelector((state)=> state.home);
     const navigate = useNavigate();
 
-    const navigation = () =>{
+    const navigation = (dir) =>{
 
+    }
+
+    const skItem = () => {
+      return(
+        <div className="skeletonItem">
+          <div className="posterBlock skeleton"></div>
+          <div className="textBlock">
+            <div className="title skeleton"></div>
+            <div className="title skeleton"></div>
+          </div>
+        </div>
+      )
     }
   return (
     <div className="carousel">
       <ContentWrapper>
         <BsFillArrowLeftCircleFill 
             className="carouselLeftNav arrow"
-            onClick={()=>{navigate("left")}}
+            onClick={()=>{navigation("left")}}
         />
         <BsFillArrowRightCircleFill
             className="carouselLeftNav arrow"
-            onClick={()=>{navigate("right")}}
+            onClick={()=>{navigation("right")}}
         />
+        {!loading ? (
+          <div className="carouselItems">
+            {data?.map((item)=>{
+              const posterUrl = item.poster_path ? 
+              url.poster + item.poster_path : PosterFallback;
+              return (
+                <div key={item.id} className="carouselItem">
+                  <div className="posterBlock">
+                    <Img src={posterUrl}/>
+                    <CircleRating rating={item.vote_average.toFixed(1)}/>
+                    <Genres data={item.genre_ids.slice(0, 2)}/>
+                  </div>
+                  <div className="textBlock">
+                    <span className="title">
+                      {item.title || item.name}
+                    </span>
+
+                    <span className="date">
+                      {dayjs(item.release_date).format("MMM D, YYYY")}
+                    </span>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        ) : (
+          <div className="loadingSkeleton">
+            {skItem()}
+            {skItem()}
+            {skItem()}
+            {skItem()}
+            {skItem()}
+          </div>
+        )}
       </ContentWrapper>
     </div>
   )
